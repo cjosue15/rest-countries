e38da7e16d8d2fab490cb90188599e2876e1b3b8
Carlos Morales <carlosjosuemn98@gmail.com>
2020-09-04 20:28:31 -0500

    se termino dark mode en local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,66 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { CountriesService } from './services/countries.service';
-
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-    data: any[];
-    regiones: any[];
-    contador: number;
-    selected: any;
-    constructor(private countriesService: CountriesService) {
-        this.data = [];
-        this.regiones = [];
-        this.contador = 10;
-        this.selected = { id: '', text: 'Filter by Region' };
-        this.getData();
+    body = document.querySelector('body');
+    constructor() {
+        const theme = localStorage.getItem('theme') || 'dark-mode';
+        this.body.classList.add(theme);
     }
 
-    ngOnInit(): void {
-        window.onscroll = () => {
-            let scrollHeight;
-            let totalHeight;
-            scrollHeight = document.body.scrollHeight;
-            totalHeight = window.scrollY + window.innerHeight;
-            if (totalHeight >= scrollHeight) {
-                // console.log(this.data.length);
-
-                if (this.contador < this.data.length) {
-                    setTimeout(() => {
-                        this.contador += 10;
-                    }, 500);
-                }
-
-                // console.log(this.contador);
-            }
-        };
-    }
-
-    async getData() {
-        try {
-            this.data = await this.countriesService.getAllCountries().toPromise();
-            this.regiones = await this.countriesService.getSelects().toPromise();
-            this.regiones.unshift({ id: 'all', text: 'All', count: this.data.length });
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    changeRegion(event) {
-        this.contador = 10;
-        this.selected = event;
-        const ruta = event.id === 'all' ? 'all' : `region/${event.id}`;
-
-        this.countriesService.getCountriesByRegion(ruta).subscribe((response) => {
-            this.data = response;
-            console.log(response);
-        });
-    }
-
-    pedirMas() {
-        this.contador += 10;
-    }
+    ngOnInit(): void {}
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,17 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
     body = document.querySelector('body');
-    constructor() {}
+    dark: boolean;
+    constructor() {
+        this.dark = false;
+        this.dark = localStorage.getItem('theme') === 'dark-mode' ? true : false;
+    }
 
     ngOnInit(): void {}
 
     changeTheme() {
         if (this.body.classList.contains('dark-mode')) {
             this.body.classList.remove('dark-mode');
-            this.body.classList.add('ligth-mode');
+            this.body.classList.add('light-mode');
+            localStorage.setItem('theme', 'light-mode');
+            this.dark = false;
         } else {
-            this.body.classList.remove('ligth-mode');
+            this.body.classList.remove('light-mode');
             this.body.classList.add('dark-mode');
+            this.dark = true;
+            localStorage.setItem('theme', 'dark-mode');
         }
     }
 }
