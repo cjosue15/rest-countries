d14605886c34ad3c7c4a753cbc0db806e0a67a38
Carlos Morales <carlosjosuemn98@gmail.com>
2020-09-04 19:55:28 -0500

    se añadio solo css de dark mode

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+    body = document.querySelector('body');
+    constructor() {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+    changeTheme() {
+        if (this.body.classList.contains('dark-mode')) {
+            this.body.classList.remove('dark-mode');
+            this.body.classList.add('ligth-mode');
+        } else {
+            this.body.classList.remove('ligth-mode');
+            this.body.classList.add('dark-mode');
+        }
+    }
 }
